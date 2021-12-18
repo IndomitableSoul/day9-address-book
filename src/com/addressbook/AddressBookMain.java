9ba3a973e01c8323fa7a68aa50daaab6bdad0c8d
nikita <amarnikita09@gmail.com>
2021-12-18 11:09:36 +0530

    Refatored AddressBook.java , Contact.java and added Address.java

--- src/com/addressbook/AddressBookMain.java
+++ src/com/addressbook/AddressBookMain.java
@@ -1,7 +1,32 @@
 package com.addressbook;
 
+
+import java.util.Scanner;
 public class AddressBookMain {
+	public static Scanner scanner = new Scanner(System.in);
+	
+	public void addContact() {
+		System.out.println("Enter first name");
+		String firstName = scanner.next();
+		System.out.println("Enter last name");
+		String lastName = scanner.next();
+		System.out.println("Enter city");
+		String city = scanner.next(); 
+		System.out.println("Enter state ");
+		String state = scanner.next();
+		System.out.println("Enter zip");
+		long zip = scanner.nextLong();
+		System.out.println("Enter phone");
+		long phone = scanner.nextLong();
+		System.out.println("Enter email");
+		String email = scanner.next();
+		
+		Contact person = new Contact(firstName, lastName, city, state, zip, phone, email);
+		System.out.println(person);
+	}
 	public static void main(String[] args) {
-		System.out.println("Welcom To Address Book Program");
+		System.out.println("Welcome To Address Book Program");
+		AddressBookMain addressBook = new AddressBookMain();
+		addressBook.addContact();	
 	}
 }
--- src/com/addressbook/Contact.java
+++ src/com/addressbook/Contact.java
@@ -9,26 +9,29 @@ public class Contact {
 	*/
 		public String firstName;
 		public String lastName;
-		public String address;
-		public String city;
-		public String state;
-		public long zip;
+		public Address address;
 		public long phoneNumber;
 		public String email;
 		
 		
-		public Contact(String firstName, String lastName, String address, String city, 
+		public Contact(String firstName, String lastName, String city, 
 						String state, long zip,long phoneNumber, String email) {
 			super();
 			this.firstName = firstName;
 			this.lastName = lastName;
-			this.address = address;
-			this.city = city;
-			this.state = state;
-			this.zip = zip;
+			this.address = new Address(city, state, zip);
 			this.phoneNumber = phoneNumber;
 			this.email = email;
 		}
+
+
+		@Override
+		public String toString() {
+			return "Contact [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address
+					+ ", phoneNumber=" + phoneNumber + ", email=" + email + "]";
+		}
+		
+		
 		
 		
 } 
